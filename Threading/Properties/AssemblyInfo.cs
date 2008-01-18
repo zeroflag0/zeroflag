2c633c0b8ac20640a1ae817dd48cd6c73a55fcbb
zeroflag0 <zeroflag0@922e9179-fa43-0410-ae15-7bdd9a12c71d>
2008-01-18 09:16:27 +0000

    Cleaned out for a beta binary release.

--- Threading/Properties/AssemblyInfo.cs
+++ Threading/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("zeroThread")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("zeroflag.Threading")]
+[assembly: AssemblyDescription("zeroflag's little Threading helper")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("zeroThread")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2008")]
+[assembly: AssemblyCompany("zeroflag")]
+[assembly: AssemblyProduct("zeroflag.Threading")]
+[assembly: AssemblyCopyright("Copyright zeroflag (c) 2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.4")]
+[assembly: AssemblyFileVersion("0.1.0.4")]
