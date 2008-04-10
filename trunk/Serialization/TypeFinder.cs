83a57ee11029165086b5fb6ee317ca1bc712c776
zeroflag0 <zeroflag0@922e9179-fa43-0410-ae15-7bdd9a12c71d>
2008-04-10 11:57:03 +0000

    Moved ZmlSerializer to zeroflag.Serialization.csproj

--- trunk/Serialization/TypeFinder.cs
+++ trunk/Serialization/TypeFinder.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace zeroflag.Zml
+namespace zeroflag.Serialization
 {
 	public class TypeFinder// : IDictionary<string, Type>, ICollection<Type>
 	{
