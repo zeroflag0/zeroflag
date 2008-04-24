81491cc2301a74ccf8280dfaf4e9b32d0c40e363
zeroflag0 <zeroflag0@922e9179-fa43-0410-ae15-7bdd9a12c71d>
2008-04-24 14:34:34 +0000

    Some work on zeroflag.Forms.Reflected.

---
+++ Forms/Reflected/TreeView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace zeroflag.Forms.Reflected
+{
+	public partial class TreeView<T> : UserControl
+	{
+		public TreeView()
+		{
+			InitializeComponent();
+		}
+
+		[Browsable(false)]
+		public System.Windows.Forms.ListView Control
+		{
+			get { return _Control; }
+		}
+	}
+}
