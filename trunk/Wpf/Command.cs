d84a0d68afb4f594e65d7dc37165f3c0708b3b11
zeroflag0@gmail.com <zeroflag0@gmail.com@922e9179-fa43-0410-ae15-7bdd9a12c71d>
2012-04-11 12:27:17 +0000

    Command

---
+++ trunk/Wpf/Command.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace zeroflag.wpf
+{
+	public class Command : ICommand
+	{
+		private Action<object> _Execute;
+		private Func<object, bool> _CanExecute;
+
+		public Command(Action<object> execute)
+			: this(execute, null, null)
+		{
+		}
+
+		public Command(Action<object> execute, Func<object, bool> canExecute)
+			: this(execute, canExecute, null)
+		{
+		}
+
+		public Command(Action<object> execute, Func<object, bool> canExecute, EventHandler canExecuteChanged)
+		{
+			_Execute = execute;
+			_CanExecute = canExecute;
+			if (canExecuteChanged != null)
+				CanExecuteChanged += canExecuteChanged;
+		}
+
+		public bool CanExecute(object parameter)
+		{
+			if (_CanExecute != null)
+			{
+				return true;
+			}
+			return _CanExecute(parameter);
+		}
+
+		public event EventHandler CanExecuteChanged;
+
+		public void Execute(object parameter)
+		{
+			if (_Execute != null)
+			{
+				_Execute(parameter);
+			}
+		}
+	}
+}
